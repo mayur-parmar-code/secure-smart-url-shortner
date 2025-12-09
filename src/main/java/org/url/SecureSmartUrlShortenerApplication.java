e41a5d845c08951eb0238c1339ff34072eaf826d
Mayur <mayur.parmar@gtcsys.com>
2025-12-09 21:02:36 +0530

    Start test

---
+++ src/main/java/org/url/SecureSmartUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package org.url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureSmartUrlShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureSmartUrlShortenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/url/SecureSmartUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureSmartUrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
